405c713c645f820baac66544c873a820f0d24824
Titomr2025 <tmartinez2019@hotmail.com>
2026-01-31 20:10:01 -0500

    Hito 7: build actualizado y configuración correcta para deploy

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Pizzer-a-Mamma/',
+  base: '/Pizzer-a-Mamma-M-a-2026/',
+  build: {
+    outDir: 'docs',
+  },
 })
